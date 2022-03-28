1ef9619f4cd7791332d9dcb96aca2ef234e7b3ca
Ramon Diaz <50044994+ramndiaz@users.noreply.github.com>
2022-03-28 11:05:42 -0400

    add focus_timer and authenticated to mongo

--- backend/src/app.py
+++ backend/src/app.py
@@ -5,7 +5,8 @@ from bson import json_util #para convertir la respuesta desde mongo en un json
 from bson.objectid import ObjectId #para buscar en mongo con el id pasando de string a object
 
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb://127.0.0.1:27017/practica1db'
+
+app.config['MONGO_URI']='mongodb://127.0.0.1:27017/practica2db'
 
 mongo = PyMongo(app)
 
@@ -33,14 +34,18 @@ def update_user(id):
     username = request.json['username']
     password = request.json['password']
     email = request.json['email']
-    if username and email and password:
+    authenticated = request.json['authenticated']
+    focus_time = request.json['focus_time']
+    if username and email and password and authenticated and focus_time:
         #por ahora validando que hay datos
         hashed_password = generate_password_hash(password)
         #escondiendo la contrasena
         mongo.db.users.update_one({'_id': ObjectId(id)}, {'$set':{
             'username': username,
             'password': hashed_password,
-            'email': email
+            'email': email,
+            'authenticated': authenticated,
+            'focus_time': focus_time
         }})
         response = jsonify({'messsage': ' User ' + id + ' was updated'})
         return response
@@ -52,22 +57,31 @@ def create_user():
     username = request.json['username']
     password = request.json['password']
     email = request.json['email']
+    authenticated = request.json['authenticated']
+    focus_time = request.json['focus_time']
     #atrapando los datos de request
-    if username and email and password:
+    if username and email and password and authenticated and focus_time:
         #por ahora validando que hay datos
         hashed_password = generate_password_hash(password)
         #escondiendo la contrasena
-        id = mongo.db.users.insert_one({'username': username, 'email': email, 'password': hashed_password})
+        id = mongo.db.users.insert_one({
+            'username': username,
+            'email': email,
+            'password': hashed_password,
+            'authenticated': authenticated,
+            'focus_time': focus_time
+            })
         #guardando los datos del request
         response = {
             'id': str(id),
             'username': username,
             'email': email,
-            'password': hashed_password
+            'password': hashed_password,
+            'authenticated': authenticated,
+            'focus_time': focus_time
         }
         return response
     else:
-    #    return {'message': 'not received'}
         return not_found()
 
 @app.errorhandler(404)
@@ -78,5 +92,7 @@ def not_found(error=None):
     })
     message.status_code=404
     return message
+
+
 if __name__=="__main__":
 	app.run(debug=True)
