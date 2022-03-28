fa47329125e22798c96619170bae272de0157195
Ramon Diaz <50044994+ramndiaz@users.noreply.github.com>
2022-03-28 12:26:18 -0400

    add test front

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld/> -->
+    <Test/>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld'
+import Test from './components/Test'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+     Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+    <table>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>user id: {{user.username}}</td>
+          <td>id: {{user.email}}</td>
+          <td>title: {{user.password}}</td>
+          <td>complete: {{user.authenticated}}</td>
+          <td>title: {{user.focus_time}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Test Component',
+      users:null
+    }
+  },
+  mounted(){
+    this.getUsers();
+  },
+  methods:{
+    getUsers(){
+      axios
+        .get('http://localhost:5000/users')
+        .then( response => {
+          this.users=response.data
+        })
+        .catch( e=> console.log(e))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.test{
+  background-color: aqua;
+  color:black;
+}
+</style>
